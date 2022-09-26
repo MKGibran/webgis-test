2cfa56396fd99422a0263b707532665ec40c1c70
Gibran <70522162+MKGibran@users.noreply.github.com>
2022-09-26 14:35:59 +0700

    layer control

--- app/Views/index.php
+++ app/Views/index.php
@@ -58,21 +58,50 @@
 
         <script>
             var map = L.map('map').setView([-6.587758969110724, 106.80593782651178], 13);
+            
+            var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+            }).addTo(map);
+
             <?php foreach($tempats as $t): ?>
               var marker = L.marker([<?= $t['longitude']; ?>, <?= $t['latitude']; ?>]).addTo(map);
               marker.bindPopup("<?= $t['nama']; ?>").openPopup();
             <?php endforeach; ?>
 
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 19,
+            var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+              attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+              subdomains: 'abcd',
+              minZoom: 1,
+              maxZoom: 16,
+              ext: 'jpg'
+              });
+            Stamen_Watercolor.addTo(map);
+
+            var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+              maxZoom: 20,
+              attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
             }).addTo(map);
 
-            // var circle = L.circle([-6.587758969110724, 106.80593782651178], {
-            //     color: 'red',
-            //     fillColor: '#f03',
-            //     fillOpacity: 0.5,
-            //     radius: 100
-            // }).addTo(map);
+            var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+              attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+              subdomains: 'abcd',
+              minZoom: 0,
+              maxZoom: 20,
+              ext: 'png'
+            }).addTo(map);
+
+            var baseLayers = {
+                "Water Color":Stamen_Watercolor,
+                "OpenStreetMap": osm,
+                "Dark Map" : Stadia_AlidadeSmoothDark,
+                "Stamen Toner" : Stamen_Toner
+            };
+
+            var overlays = {
+                "Marker": marker,
+            };
+
+            L.control.layers(baseLayers, overlays).addTo(map);
         </script>
         
         <?= $this->endSection() ?>
